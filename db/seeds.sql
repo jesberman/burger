9a0971704d727614d2573be6933302b73e89a2c7
jesberman <jesberman@gmail.com>
2018-08-19 10:32:52 -0400

    firts_commit

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ("Bacon Lettuce and Tomato", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Angus Steak", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Chipotle", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("The Luther", false);

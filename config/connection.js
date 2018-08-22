819cd1369a8974b931f4b16c55ef23921c1b5a03
jesberman <jesberman@gmail.com>
2018-08-22 17:38:57 -0400

    new-commit

---
+++ config/connection.js
@@ -0,0 +1,25 @@
+
+var mysql = require('mysql');
+
+var connection = mysql.createConnection({
+    host: "localhost",
+
+    // Port
+    port: 3306,
+
+    //  username
+    user: "root",
+
+    //  password
+    password: "Bloodraven36",
+    database: "burgers_db"
+});
+
+
+connection.connect(function (err) {
+    if (err) throw err;
+    //runs the "afterConnection" function
+    console.log("connected as id " + connection.threadId);
+});
+
+module.exports = connection;
--- server.js
+++ server.js
@@ -2,3 +2,27 @@ var express = require("express");
 //var exphbs = require("express-handlebars");
 //var mysql = require("mysql");
 var bodyParser = require('body-parser');
+
+var app = express();
+
+
+// Set Handlebars.
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+
+
+var PORT = process.env.PORT || 8080;
+
+
+
+// Serve static content for the app from the "public" directory in the application directory.
+app.use(express.static("public"));
+
+
+// Import routes and give the server access to them.
+var routes = require("./controllers/burgersController.js");
+
+app.use(routes);

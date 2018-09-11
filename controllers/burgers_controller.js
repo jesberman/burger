f83a7d42b3aaf249ee9a110cc69f1e0606cf8d7e
jesberman <jesberman@gmail.com>
2018-09-10 23:05:57 -0400

    working-on-update-route

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -8,17 +8,28 @@ router.get("/", function (req, res) {
         var hbsObject = {
             burgers: data
         };
-             console.log(hbsObject);
+        console.log(hbsObject);
         res.render("index", { burgers: data });
     });
 });
 
-router.post("/newBurger", function (req, res) {
-    burger.create();
-}
+router.post("/api/new", function (req, res) {
+    console.log(req.body);
+    burger.create(["burger_name", "devoured"], [
+        req.body.burger_name, "0"
+    ], function (result) {
+        res.json({ id: result.insertId });
+    });
+});
 
-
-)
+router.put("/api/devour/:id", function (req, res) {
+    console.log(req.body);
+    // burger.update(["devoured"], ["1"], "WHERE id =  " + req.params.ids,
+    burger.update({devoured: true},"WHERE id =  " + req.params.id), 
+        function (result) {
+            res.json({ id: result.insertId });
+        };
+});
 
 module.exports = router;
 
